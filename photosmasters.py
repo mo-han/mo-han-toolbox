32e2dcbea5a0d7e87fddc5803bb35b8bdfc77e49
mo-han <zmhungrown@gmail.com>
2018-01-09 03:44:31 +0800

    photosmasters.py: rewrite, rename, multi-thread.

--- photosmasters.py
+++ photosmasters.py
@@ -2,8 +2,9 @@
 
 from re import sub
 from urllib.parse import urlparse, parse_qsl
-import logging
 from time import sleep
+from threading import Thread
+import logging
 import sys
 import os
 import argparse
@@ -12,7 +13,7 @@ import json
 from requests import get, head
 from bs4 import BeautifulSoup
 
-from lib_misc import ExitCode, LOG_FMT, LOG_FMT_MESSAGE_ONLY
+from lib_misc import win32_ctrl_c, ExitCode, LOG_FMT, LOG_FMT_MESSAGE_ONLY
 
 DB_FILE = 'db.json'
 INFO_FILE = 'info.txt'
@@ -21,29 +22,36 @@ __program__ = 'photosmasters'
 __description__ = 'Album photos downloader for photosmasters.com'
 _logger = logging.getLogger('photosmasters')
 
+db = {}
+stat = {}
+server = ''
+thl = []
+
 
 def parse_args():
     ap = argparse.ArgumentParser(description=__description__)
     ap.add_argument('dir', help='download directory')
     ap.add_argument('url', help='album URL of photosmasters.com')
-    ap.add_argument('-v', '--verbose', action='store_true',)
+    ap.add_argument('--threads', '-t', type=int, default=5, help='number of download threads')
+    ap.add_argument('-v', '--verbose', action='store_true', )
     return ap.parse_args()
 
 
-def restore_db(db: dict, file: str):
+def restore_db(file: str):
+    global db
     if os.path.exists(file):
         with open(file, 'r') as f:
             db = json.load(f)
         _logger.info('Restore database from {}'.format(file))
 
 
-def save_db(db: dict, file: str):
+def save_db(file: str):
     with open(file, 'w') as f:
         json.dump(db, f)
     _logger.info('Save database to {}'.format(file))
 
 
-def save_info(stat: dict, file: str):
+def save_info(file: str):
     with open(file, 'w') as f:
         f.write(stat['at'])
     _logger.info('Save info to {}'.format(file))
@@ -67,34 +75,56 @@ def content_length(respond):
     return int(respond.headers['Content-Length'])
 
 
-def download(pid: str, file: str, db: dict):
+def image_from_photo(pid: str, purl: str, pdb: dict):
+    global server
+    photo_soup, _ = soup(purl)
+    if pdb:
+        img_url = pdb['url']
+    else:
+        img = photo_soup.img
+        img_url = server + '/' + img['src']
+        img_ext = os.path.splitext(img_url)[-1]
+        img_alt = img['alt']
+        img_size = None
+        pdb = {'url': img_url, 'ext': img_ext, 'alt': img_alt, 'size': img_size, }
+        db[pid] = pdb
+    _logger.debug('Image: {}'.format(img_url))
+
+
+def download(pid: str, purl: str, name: str):
+    global server
+    dl_dir = stat['dd']
+    pdb = db.get(pid)
+    image_from_photo(pid, purl, pdb)
     while True:
         try:
             pdb = db[pid]
-            url = pdb['src']
+            pdb['name'] = name
+            file = os.path.join(dl_dir, '{} - {}{}'.format(name, pid, pdb['ext']))
+            _logger.debug('{} <-> {}'.format(pdb, file))
+            url = pdb['url']
             if os.path.exists(file):
                 local_size = os.stat(file).st_size
                 pdb_remote_size = pdb['size']
                 if pdb_remote_size:
-                    if local_size == pdb_remote_size:
-                        _logger.info('Finish: {}'.format(file))
-                        break
+                    remote_size = pdb_remote_size
                 else:
                     remote_size = content_length(head(url))
-                    if remote_size == local_size:
-                        pdb['size'] = remote_size
-                        _logger.info('Finish: {}'.format(file))
-                        break
-            else:
-                r = get(url)
-                if r.status_code == 200:
-                    with open(file, 'wb') as f:
-                        f.write(r.content)
-                    pdb['size'] = content_length(r)
-                    _logger.info('Download: {} -> {}'.format(url, file))
+                    pdb['size'] = remote_size
+                if local_size == remote_size:
+                    _logger.info('Good: {}'.format(file))
                     break
                 else:
-                    _logger.warning('Retry: {}'.format(url))
+                    _logger.warning('Bad: {}'.format(file))
+            r = get(url)
+            if r.status_code == 200:
+                with open(file, 'wb') as f:
+                    f.write(r.content)
+                pdb['size'] = content_length(r)
+                _logger.info('OK: {} -> {}'.format(url, file))
+                break
+            else:
+                _logger.warning('Fail: {}'.format(url))
         except Exception as e:
             _logger.error(repr(e))
 
@@ -112,61 +142,14 @@ def get_query_dict(url: str):
     return dict(parse_qsl(query))
 
 
-def display_stat(stat: dict):
-    _logger.info('[{li}/{ti}][{lp}/{tp}] {at}'.format(**stat))
+def display_stat():
+    _logger.info('[{li}/{ti}][{lp}/{tp}] {at} -->> {dd}'.format(**stat))
 
 
-def gn_pages_per_album(url: str, stat: dict):
-    album_soup, _ = soup(url)
-
-    album_title = album_soup.title.contents[0]
-    _logger.info(album_title)
-    _logger.info(url)
-
-    totals = album_soup('td', class_='tableh1')[0].contents[0]
-    _, total_images, _, _, total_pages = totals.split()
-    stat.update({
-        'at': album_title,  # title
-        'ti': int(total_images),  # total images
-        'tp': int(total_pages),  # total pages
-        'li': 0,  # last image
-        'lp': 0,  # last page
-    })
-    display_stat(stat)
-
-    for page in range(stat['tp']):
-        stat['lp'] += 1
-        page_url = '{}&page={}'.format(url, stat['lp'])
-        _logger.debug('Page: {}'.format(page_url))
-        yield page_url
-
-
-def gn_photos_per_page(url: str, stat: dict, db: dict):
-    server, _ = url_split(url)
-    page_soup, _ = soup(url)
-    for thumbnail in page_soup('td', class_='thumbnails'):
-        stat['li'] += 1
-        photo_url = server + '/' + thumbnail.a['href'].replace('#top_display_media', '&fullsize=1')
-        _logger.debug('Photo: {}'.format(photo_url))
-        photo_id = get_query_dict(photo_url)['pid']
-        photo_soup, _ = soup(photo_url)
-        photo_db = db.get(photo_id)
-        if photo_db:
-            img_src = photo_db.get('src')
-        else:
-            img = photo_soup.img
-            img_src = server + '/' + img['src']
-            img_alt = img['alt']
-            img_size = None
-            photo_db = {'src': img_src, 'alt': img_alt, 'size': img_size}
-            db[photo_id] = photo_db
-        _logger.debug('Image: {}'.format(img_src))
-        yield photo_id
-
-
-def main(url: str=None):
+def main(url: str = None):
+    # win32_ctrl_c()
     args = parse_args()
-
+    # Get verbose level from args
     if args.verbose:
         lvl = logging.DEBUG
         fmt = LOG_FMT
@@ -178,38 +161,74 @@ def main(url: str=None):
         level=lvl,
         format=fmt,
     )
-
+    # Get URL from args
     if url:
         album_url = url
     else:
         album_url = args.url
-
+    global server
+    server, _ = url_split(album_url)
+    # Get album directory to store downloaded photos
     album_id = get_query_dict(album_url)['album']
     album_dir = os.path.join(args.dir, 'photosmasters', album_id)
+    if not os.path.exists(album_dir):
+        os.makedirs(album_dir)
+    _logger.info('{} -->> {}'.format(album_url, album_dir))
+    # Meta
     info_file = os.path.join(album_dir, INFO_FILE)
     db_file = os.path.join(album_dir, DB_FILE)
-    stat = {}
-    db = {}
-
+    restore_db(db_file)
+    # Parse album page
+    album_soup, _ = soup(album_url)
+    album_title = album_soup.title.contents[0]
+    _logger.info(album_title)
+    totals = album_soup('td', class_='tableh1')[0].contents[0]
+    _, total_images, _, _, total_pages = totals.split()
+    global stat
+    stat = {
+        'dd': album_dir,  # download directory
+        'at': album_title,  # title
+        'ti': int(total_images),  # total images
+        'tp': int(total_pages),  # total pages
+        'li': 0,  # last image
+        'lp': 0,  # last page
+    }
+    display_stat()
+    # Let's roll out
     try:
-        if not os.path.exists(album_dir):
-            os.makedirs(album_dir)
-        restore_db(db, db_file)
-        _logger.info('Directory: {}'.format(album_dir))
-
-        for page_url in gn_pages_per_album(album_url, stat):
-            for photo_id in gn_photos_per_page(page_url, stat, db):
-                pdb = db[photo_id]
-                file_name = '{}-{}'.format(photo_id, pdb['alt'])
-                file_path = os.path.join(album_dir, file_name)
-                display_stat(stat)
-                download(photo_id, file_path, db)
-
+        for _ in range(stat['tp']):
+            stat['lp'] += 1
+            page_url = '{}&page={}'.format(album_url, stat['lp'])
+            _logger.debug('Page: {}'.format(page_url))
+            page_soup, _ = soup(page_url)
+            thumbnails = page_soup('td', class_='thumbnails')
+            for thumb in thumbnails:
+                if not thumb.td:
+                    break
+                stat['li'] += 1
+                thumb_title = thumb.td.span.contents[0]
+                photo_url = server + '/' + thumb.a['href'].replace('#top_display_media', '&fullsize=1')
+                _logger.debug('Photo: {}'.format(photo_url))
+                photo_id = get_query_dict(photo_url)['pid']
+                # Go!
+                th = Thread(target=download, args=(photo_id, photo_url, thumb_title))
+                while len(thl) >= args.threads:
+                    for t in thl:
+                        if not t.is_alive():
+                            thl.remove(t)
+                    sleep(0.1)
+                display_stat()
+                th.start()
+                thl.append(th)
+                sleep(0.1)
     except KeyboardInterrupt:
+        _logger.info('Wait: {} threads to finish.'.format(len(thl)))
         sys.exit(ExitCode.CTRL_C)
     finally:
-        save_db(db, db_file)
-        save_info(stat, info_file)
+        for th in thl:
+            th.join()
+        save_db(db_file)
+        save_info(info_file)
 
 
 if __name__ == '__main__':
