fbfcebaf8ac205cfeebfc58f9ef94383dffb477d
mo-han <zmhungrown@gmail.com>
2018-04-01 22:10:18 +0800

    nhentai: add lib_hentai.NHentaiKit, add nhentai

--- lib_hentai.py
+++ lib_hentai.py
@@ -1,19 +1,129 @@
 #!/usr/bin/env python
 
-from bs4 import BeautifulSoup
+import os
 import requests
 import logging
 import re as regex
 import json
 import zipfile
+from bs4 import BeautifulSoup
+from time import sleep
 from multiprocessing.dummy import Pool
 # import platform
 
 from lib_misc import rectify_path_char
-# if platform.system() == 'Linux':
-#     from multiprocessing import Pool
-# else:
-#     from multiprocessing.dummy import Pool
+
+
+_requests_session = requests.Session()
+_requests_session.headers['User-Agent'] = \
+    'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) ' \
+    'AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
+
+
+class HentaiException(Exception):
+    pass
+
+
+class HentaiRequestError(HentaiException):
+    pass
+
+
+class HentaiParseError(HentaiException):
+    pass
+
+
+class HentaiDownloadError(HentaiException):
+    pass
+
+
+def get_lxml_soup(r: requests.Response, *args, **kwargs):
+    return BeautifulSoup(r.content, 'lxml', *args, **kwargs)
+
+
+def http_get(url: str, head_only: bool = False, max_retries: int = 3, retry_interval: float = 5.0, ):
+    """Wrapper of requests.get, with some extra param."""
+    if head_only:
+        req = _requests_session.head
+    else:
+        req = _requests_session.get
+    error_args = ()
+    while max_retries > 0:
+        try:
+            r = req(url)
+            if r.ok:
+                return r
+            else:
+                raise HentaiRequestError('Bad response.', url, r.status_code, r.reason)
+        except requests.RequestException as e:
+            error_args = e.args + error_args
+            max_retries -= 1
+            sleep(retry_interval)
+    else:
+        raise HentaiRequestError('Connection failed, out of retries.', url, *error_args)
+    pass
+
+
+class NHentaiKit:
+    def __init__(self, max_dl: int = 10):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.__max_dl = max_dl
+        self.logger.info('{} downloading threads.'.format(self.__max_dl))
+        self.__dl_pool = Pool(self.__max_dl)
+
+    @property
+    def dl_pool(self):
+        return self.__dl_pool
+
+    def get_gallery(self, url: str):
+        self.logger.info(url)
+        soup = get_lxml_soup(http_get(url))
+        info = soup.find('div', id='info')
+        h1 = info.h1.text
+        self.logger.info(h1)
+        h2 = info.h2.text
+        self.logger.info(h2)
+        # src_id = soup.find('div', id='cover').img['data-src'].rsplit('/', 2)[-2]
+        # gallery_path = url.split('://')[0] + '://i.nhentai.net/galleries/' + src_id
+        # page_num = int(soup.find('div', id='info')('div')[-3].text.split(' pages')[0])
+        # self.__logger.info('{} pages in {}'.format(page_num, gallery_path))
+        all_thumb_url_l = [i.img['data-src'] for i in soup('a', class_='gallerythumb')]
+        all_pic_url_l = [regex.sub(r'(.*)t.nhentai.net(.*)t.(.*)', r'\1i.nhentai.net\2.\3', s) for s in all_thumb_url_l]
+        return h1, all_pic_url_l
+
+    def download_picture(self, picture_url: str):
+        try:
+            p_name = picture_url.rsplit('/', 1)[-1]
+            p_get = http_get(picture_url)
+            p_src_size = int(p_get.headers['Content-Length'])
+            p_data = p_get.content
+            p_dl_size = len(p_data)
+            self.logger.info('{} ({}/{}) {}'.format(p_name, p_dl_size, p_src_size, picture_url))
+            if p_dl_size == p_src_size:
+                return p_name, p_data
+            else:
+                raise HentaiDownloadError('Incomplete data', picture_url, p_src_size, p_dl_size)
+        except HentaiRequestError as e:
+            raise HentaiDownloadError(*e.args)
+
+    def save_gallery_to_cbz(self, gallery_src: tuple, dl_dir: str = ''):
+        if dl_dir:
+            self.logger.info('Download to dir: {}'.format(dl_dir))
+        title, all_pic_url_list = gallery_src
+        cbz_name = rectify_path_char(title) + '.cbz'
+        self.logger.info('Save to file: {}'.format(cbz_name))
+        cbz_path = os.path.join(dl_dir, cbz_name)
+        err_log_path = cbz_path + '.error.log'
+        with zipfile.ZipFile(cbz_path, 'w') as cbz:
+            self.logger.debug(cbz)
+            for pic_name, pic_data in self.dl_pool.imap_unordered(self.download_picture, all_pic_url_list):
+                try:
+                    cbz.writestr(pic_name, pic_data)
+                except HentaiDownloadError as e:
+                    with open(err_log_path, 'a') as error_log:
+                        error_log.write(str(e))
+                        error_log.write('\n')
+                        error_log.close()
+            cbz.close()
 
 
 def get_soup(uri: str, parser: str = 'lxml', retry: int = 3):
@@ -30,14 +140,6 @@ def get_soup(uri: str, parser: str = 'lxml', retry: int = 3):
             return False
         finally:
             retry -= 1
-
-
-class HentaiException(Exception):
-    pass
-
-
-class HentaiDownloadError(HentaiException):
-    pass
 
 
 class HentaiCafeKit:
