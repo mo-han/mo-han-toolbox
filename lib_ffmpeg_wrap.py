d56a849f989f102a859ec61656a55690e22a9dd8
mo-han <zmhungrown@gmail.com>
2019-01-04 00:54:33 +0800

    bugfix: remove `sorted` in lib_ffmpeg_wrap.concat_videos

--- lib_ffmpeg_wrap.py
+++ lib_ffmpeg_wrap.py
@@ -9,13 +9,14 @@ def images_to_video(images_folder: str, output_video: str == None):
     pass
 
 
-def concat_videos(*args):
+def concat_videos(*args) -> None:
+    """concat_videos(part1, part2, part3, ... , output)"""
     input_list = args[:-1]
     output_path = args[-1]
     work_dir, _ = os.path.split(os.path.realpath(output_path))
     list_path = os.path.join(work_dir, '###-ffmpeg-concat-list-temp.txt')
     with open(list_path, 'w+b') as lf:
-        for i in sorted(input_list):
+        for i in input_list:
             lf.write("file '{}'{}".format(i, os.linesep).encode())
     os.system(
         'ffmpeg -n -hide_banner -loglevel +level -f concat -safe 0 -i "{}" -c copy "{}"'
