6e445cb696433280a689b940171dd16c595efe11
mo-han <zmhungrown@gmail.com>
2019-05-15 16:20:54 +0800

    *hentai_cafe_wrap.bat, +hentaicafe-dl.bat

--- hentai_cafe_wrap.bat
+++ hentai_cafe_wrap.bat
@@ -1,8 +1,12 @@
 @echo off
 setlocal
 set "hc=%~dp0hentai_cafe.py"
-title hentai_cafe_wrap.bat %u_dl% %*
+title hentai.cafe dl %u_dl% %*
+pushd %u_dl%
+set https_proxy=%u_proxy%
+set http_proxy=%u_proxy%
 
 :start
 %hc% %* || goto :start
-exit
+popd
+goto :eof
---
+++ hentaicafe-dl.bat
@@ -0,0 +1,3 @@
+@echo off
+
+start "" /min cmd /c hentai_cafe_wrap.bat %*
