21c2d15a16d11bee22ced80da6e8b5ac2be629fa
mo-han <zmhungrown@gmail.com>
2019-12-13 15:08:04 +0800

    minor modification

--- etc/nginx/install-snippets.sh
+++ etc/nginx/install-snippets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 pushd "$(dirname "$(realpath "$0")")"
-install -m 644 snippets/* /etc/nginx/snippets/
+install -v -m 644 snippets/* /etc/nginx/snippets/
+install -v -m 664 sites-available/* /etc/nginx/sites-available/
 popd
