af2de377ab36a4bd6cfc603c92aae947875ebcf8
墨焓 <zmhungrown@gmail.com>
2020-03-20 20:06:33 +0800

    + jijidown_rename.py

--- install-to-locallib_env.bat
+++ install-to-locallib_env.bat
@@ -1,4 +1,5 @@
 copy /y bilibili_appcache.py %locallib_env%\_winbin\
+copy /y jijidown_rename.py %locallib_env%\_winbin\
 copy /y lib_*.py %locallib_env%\_winbin\
 copy /y nhentai.py %locallib_env%\_winbin\
 copy /y hentai_cafe.py %locallib_env%\_winbin\
---
+++ jijidown_rename.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from glob import glob
+
+from lib_bilibili import jijidown_rename_alpha
+
+if __name__ == '__main__':
+    # _, base = os.path.split(sys.argv[1])
+    if sys.argv[1][-1] == '*':
+        args = glob(sys.argv[1])
+    else:
+        args = sys.argv[1:]
+    for i in args:
+        jijidown_rename_alpha(i)
