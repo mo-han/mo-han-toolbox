029f80a393b5c0e120e85e6a29b13f5f41dd4e4b
mo-han <zmhungrown@gmail.com>
2020-06-18 20:20:01 +0800

    clear auia

--- mylib/auia.py
+++ mylib/auia.py
@@ -3,11 +3,3 @@
 """Android UI Automate"""
 import uiautomator2
 import ppadb.client
-
-
-class AUIABase:
-    def __init__(self, remote_addr: str):
-        self._addr = remote_addr
-        self._device =
-
-    def adb_connect_remote(self, max_try):
