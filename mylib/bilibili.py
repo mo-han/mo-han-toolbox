4cf40f492f36069a7ed89aae5958372ba5006dfe
mo-han <zmhungrown@gmail.com>
2020-06-26 17:18:30 +0800

    refine comments

--- mylib/bilibili.py
+++ mylib/bilibili.py
@@ -11,7 +11,8 @@ from http.cookiejar import MozillaCookieJar
 
 import requests
 # 下面导入的是通过pip安装的you-get
-# 故意导入了子模块，是因为需要子路径，否则，如果只导入you-get这个包的话，子路径不会自动“展开”
+# 如果仅仅`import you_get`，模块名空间`sys.modules`中不会包含`you_get.util.strings`
+# 但`you_get.util.strings`这个路径在后续用得到，you-get的另外几个模块需要从这个路径导入函数
 import you_get.util.strings
 from lxml import html
 
@@ -36,13 +37,12 @@ def _tmp(avid, cid):
     return r.json()
 
 
-# 下面这个函数用于修改you-get的B站下载模块`you_get.extractors.bilibili`的源码
+# 这个函数用于修改you-get的B站下载模块`you_get.extractors.bilibili`的源码
 def code_modify_you_get_bilibili(x: str):
-    # `x`是输入的源代码字符串，就是原模块的代码，直接对其进行替换，以此修改源代码的文本
-    # 这里用的方法是在要替换的代码的上下文找到特征明显的一段，然后直接replace
+    # `x`是输入的源代码字符串，就也是原版模块的源代码
+    # 对其进行替换，在要替换的代码的上下文中，截取特征明显的一段，然后直接replace
 
-    # 下面是给尚未支持B站4K的当前版本，加上4K相关的视频流信息
-    # 其实就是在原来的dict对象中加了一项
+    # 下面这段是添加4K视频的支持，若当前版本缺少4K视频流元数据， 其实就是在原来的字典开头加了一项
     x = x.replace('''
     stream_types = [
         {'id': 'flv_p60', 'quality': 116, 'audio_quality': 30280,
@@ -54,9 +54,9 @@ def code_modify_you_get_bilibili(x: str):
         {'id': 'flv_p60', 'quality': 116, 'audio_quality': 30280,
          'container': 'FLV', 'video_resolution': '1080p', 'desc': '高清 1080P60'},
 ''')
-    # 下面也是跟4K相关的判断条件，B站视频流用不同数字ID标定不同的格式码率
-    # 120是4K，112则是大会员的1080P+（即较高码率的1080P30）
-    # （160是大会员1080P60，这里的160不需要判断，所以没有）
+    # 下面也是跟4K相关的判断代码
+    # B站视频流用不同数字ID标定不同的格式码率，120是4K，112则是大会员的1080P+（即较高码率的1080P30）
+    # （160是大会员1080P60，这里的不需要对160作判断）
     x = x.replace('''
         elif height <= 1080 and qn <= 80:
             return 80
@@ -70,14 +70,14 @@ def code_modify_you_get_bilibili(x: str):
         else:
             return 120
 ''')
-    # 下面这段修改了原本代码中的一行打印提示文本，是为了和自己写的命令行工具的选项一致
+    # 下面这段修改了原本代码中的一行提示打印文本，以便和自己写的命令行工具所设计的选项保持一致
     x = x.replace('''
                 log.w('This is a multipart video. (use --playlist to download all parts.)')
 ''', r'''
                 sys.stderr.write('# multi-part video: use -p to download other part(s)\n')
 ''')
-    # 下面这段修改了下载文件名的格式，原版是视频标题+分p子标题
-    # 我则是在视频标题+分p子标题的基础上，插入了一些有用的元信息：[av号][BV号][上传者用户名]
+    # 下面这段修改了下载文件名的格式，原版是视频标题+选集子标题
+    # 在视频标题+选集子标题的基础上，插入了一些有用的元信息：[av号][BV号][上传者用户名]
     x = x.replace('''
             # set video title
             self.title = initial_state['videoData']['title']
@@ -101,7 +101,7 @@ def code_modify_you_get_bilibili(x: str):
     # 下面这段是个重点，修改的是原版中`you_get.extractors.bilibili.Bilibili.prepare_by_url`这个方法函数
     # 原版you-get对相当多的B站视频无法获取大会员的1080P+、1080P60等格式
     # 原版这里的逻辑有问题，按下面这样修改后，用到现在没发现异常
-    # 究竟是什么原因？我没有彻底搞明白，但肯定与原版代码中`current_quality`和`best_quality`有关
+    # 根本原因没有彻底搞明白，但肯定与原版代码中`current_quality`和`best_quality`的判定有关
     x = x.replace('''
             # get alternative formats from API
             for qn in [112, 80, 64, 32, 16]:
@@ -123,8 +123,8 @@ def code_modify_you_get_bilibili(x: str):
     # （其实加这个方法，还是因为Bug修得不彻底，权宜之计罢了）
     # 顺便一提，这个`del_unwanted_dash_streams`方法不是原版you-get代码里自带的
     # 而是在新的`YouGetBilibiliX`类里自定义的方法
-    # 而`YouGetBilibiliX`则是继承的魔改版`bilibili.Bilibili`
-    # 替换原版代码 -> 调用一个原版没有的方法 -> 魔改版 -> 继承魔改版的新类 -> 在新类中补上这个缺失的方法
+    # 而`YouGetBilibiliX`则是继承的修改版`bilibili.Bilibili`
+    # 替换原版代码 -> 调用一个原版没有的方法 -> 修改版 -> 继承修改版的新类 -> 在新类中补上这个缺失的方法
     # 回溯套娃，左右横跳！
     x = x.replace('''
     def prepare_by_cid(self,avid,cid,title,html_content,playinfo,playinfo_,url):
@@ -139,7 +139,7 @@ def code_modify_you_get_bilibili(x: str):
 # 下面这个函数用于修改you-get的文件系统模块`you_get.util.fs`的源码
 def code_modify_you_get_fs(x: str):
     # 原版为了兼容VFAT文件系统，会把文件名里面的方括号替换成圆括号
-    # 魔改版不需要这个特性，所以将这两行代码加#注释掉了
+    # 修改版不需要这个特性，所以将这两行代码加#注释掉了
     x = x.replace("ord('['): '(',", "#ord('['): '(',")
     x = x.replace("ord(']'): ')',", "#ord(']'): ')',")
     # 下面把最大文件名字符串长度从80延长到200，原版的80有点小，200会不会太大尚不清楚
@@ -151,22 +151,21 @@ def code_modify_you_get_fs(x: str):
     return x
 
 
-# 上面已经导入了原版的`you_get.util.strings`
-# 对python，此时原版的`you_get`及其下级子路径都已经注册在模块名空间中了
-# 在此基础上，下面一行代码将模块名空间中的原版的`you_get.util.fs`替换成经过修改的新模块
+# 上面已经导入了原版的`you_get.util.strings`，这条模块路径很重要，另有几个模块依赖它
+# 在此基础上，下面一行代码将原版的`you_get.util.fs`替换成修改版
 you_get.util.fs = modify_and_import('you_get.util.fs', code_modify_you_get_fs)
-# 接着把`you_get.util.strings.legitimize`这个原版的函数替换成修改后的模块`you_get.util.fs`中的魔改版函数
-# 顺便一提，上面的`code_modify_you_get_fs`修改的源码就是`legitimize`这个函数的源码
+# 接着把原版中的`you_get.util.strings.legitimize`函数替换成修改版`you_get.util.fs`中的对应函数
+you_get.util.strings.legitimize = you_get.util.fs.legitimize
+# （上面的`code_modify_you_get_fs`修改的就是`legitimize`的源码）
 # 在原版的you-get中，`.util.string`从`.util.fs`中导入了`legitimize`这个函数
 # `.util.string`又利用已经导入的`legitimize`和其他几个函数，构建了一个`get_filename`函数
 # 而`you_get.extractor`和`you_get.common`又都用到了`get_filename`，当然是各自分别从`you_get.util.strings`导入的
 # 因此，所以，故而，然则，
-# 只要将原版`you_get.util.strings`中的`legitimize`替换成魔改版的`you_get.util.fs`中的`legitimize`函数即可
-# 其他从``you_get.util.strings`二次导入这个函数的模块会自动导入已经被替换成魔改版的函数
-you_get.util.strings.legitimize = you_get.util.fs.legitimize
+# 只要将原版`you_get.util.strings.legitimize`替换成修改版的`you_get.util.fs.legitimize`即可
+# 那些从``you_get.util.strings`二次导入`legitimize`函数的模块会自动导入已被替换的修改版
 # 综上所述，下面这行可以注释掉了
 # you_get.extractor.get_filename = you_get.common.get_filename = you_get.util.strings.get_filename
-# 下面则是将B站下载模块替换成魔改版本，所用的源码替换函数是上面之前提到的`code_modify_you_get_bilibili`
+# 下面则是将B站下载模块替换成修改版，所用的源码替换函数是`code_modify_you_get_bilibili`
 you_get.extractors.bilibili = modify_and_import('you_get.extractors.bilibili', code_modify_you_get_bilibili)
 
 
@@ -191,8 +190,9 @@ def get_vid(x: str or int) -> str or None:
     return vid
 
 
-# YouGetBilibiliX继承了`you_get.extractors.bilibili.Bilibili`，添加了一些新的功能
-# 虽然原版`Bilibili`是被继承的类，但它也是可以调用继承后新加的属性的
+# `YouGetBilibiliX`继承了`you_get.extractors.bilibili.Bilibili`，添加了一些新的功能
+# 其中包含了`del_unwanted_dash_streams`这个新方法
+# 但是对`del_unwanted_dash_streams`的调用却是在被继承的`Bilibili`（的修改版）中进行的
 class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
     def __init__(self, *args, cookies: str or dict = None, qn_max=116, qn_single=None):
         super(YouGetBilibiliX, self).__init__(*args)
@@ -203,8 +203,8 @@ class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
         self.qn_single = qn_single
         self.html = None, None
 
-    # B站视频的音频流分不同档次，选择中档128kbps的足够了，也可以强制选择最高音质
-    # 低档30216码率偏低，30232约128kbps，30280可能是320kbps也可能是128kbps，貌似跟是否4K有关，不是特别清楚
+    # B站视频的音频流分不同档次，选择中档128kbps就足够了，也可以选择最高音质
+    # 低档30216码率偏低，30232约128kbps，30280可能是320kbps也可能是128kbps，貌似跟4K有关，尚不确定
     def set_audio_qn(self, qn):
         for d in self.stream_types:
             d['audio_quality'] = qn
@@ -219,7 +219,7 @@ class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
             doc = html.document_fromstring(r.text)
             self.html = url, doc
 
-    # 设置cookies，大会员用得
+    # 设置cookies，大会员用得着
     # `cookie_str_from_dict`和`cookie_str_from_dict`这两个函数另有定义
     # 前者将cookies字典变成单字符串，后者负责读取cookies文件
     def set_cookie(self, cookies: str or dict):
@@ -274,7 +274,7 @@ class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
     def get_author_label(self, fmt='[{}]'):
         return fmt.format(self.get_author())
 
-    # 删除不需要的视频流，限定最高画质，选择下载画质
+    # 根据限定的最高画质或者选择的下载画质，从解析得到的视频流中，删除多余的、用不到的
     def del_unwanted_dash_streams(self):
         format_to_qn_id = {t['id']: t['quality'] for t in self.stream_types}
         for f in list(self.dash_streams):
@@ -283,12 +283,13 @@ class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
                 del self.dash_streams[f]
 
 
-# 这是一个任务函数，包装了魔改版的you-get的B站下载功能，供另外编写的命令行工具调用
+# 这是一个任务函数，包装了修改版的you-get的B站下载功能，供另外编写的命令行工具调用
 def download_bilibili_video(url: str or int,
                             cookies: str or dict = None, output: str = None, parts: list = None,
                             qn_max: int = None, qn_single: int = None, moderate_audio: bool = True, fmt=None,
                             info: bool = False, playlist: bool = False, caption: bool = True,
                             **kwargs):
+    # 确保在Windows操作系统中，SIGINT信号能够被传递到下层扩展中，从而确保Ctrl+C能够立即停止程序
     ensure_sigint_signal()
     dr = SimpleDrawer(sys.stderr.write, '\n')
 
