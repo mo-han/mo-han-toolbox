6fdd506e2adb0437e85e7b39d41d29e97e0c657a
mo-han <zmhungrown@gmail.com>
2020-07-13 07:49:36 +0800

    pure digit id

--- mylib/pornhub.py
+++ mylib/pornhub.py
@@ -5,6 +5,8 @@ from .text import regex_find
 
 
 def find_url_in_text(text: str) -> list:
-    p = r'/view_video\.php\?viewkey=ph[0-9a-f]+'
-    return ['https://www.pornhub.com' + e for e in regex_find(p, text, dedup=True)]
-
+    prefix = 'https://www.pornhub.com'
+    tmp = []
+    tmp.extend(regex_find(r'/view_video\.php\?viewkey=ph[0-9a-f]+', text, dedup=True))
+    tmp.extend(regex_find(r'/view_video\.php\?viewkey=\d+', text, dedup=True))
+    return [prefix + e for e in tmp]
