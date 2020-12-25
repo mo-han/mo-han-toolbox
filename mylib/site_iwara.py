e1ca1d6968532747b66488909a409ce246d5e699
mo-han <zmhungrown@gmail.com>
2020-12-25 10:33:34 +0800

    ! iwara id regex include uppercase

--- mylib/site_iwara.py
+++ mylib/site_iwara.py
@@ -10,7 +10,7 @@ from .web_client import get_html_element_tree
 
 def find_url_in_text(text: str) -> list:
     prefix = 'https://iwara.tv'
-    pattern = '/videos/[0-9a-z]+'
+    pattern = '/videos/[0-9a-zA-Z]+'
     urls = [prefix + e for e in regex_find(pattern, text, dedup=True) if 'thumbnail' not in e]
     return urls
 
