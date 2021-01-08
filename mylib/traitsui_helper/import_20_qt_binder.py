ac1bf0b66c5bd08973f431e2e8dd22ceafe17d68
mo-han <zmhungrown@gmail.com>
2021-01-08 13:28:41 +0800

    + traitsui_helper: qt_binder

---
+++ mylib/traitsui_helper/import_20_qt_binder.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# encoding=utf8
+from qt_binder.api import *
+from qt_binder.raw_widgets import *
+from qt_binder.widgets import *
+
+assert Bound
+assert Widget
+assert binder_registry
