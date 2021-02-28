9771c5643a80e0d620773cbd2d0dffd043ca3ad2
mo-han <zmhungrown@gmail.com>
2021-02-28 18:38:08 +0800

    * QtWebPageRender not subclass of QWebEnginePage

--- mylib/units/qt_web_render.py
+++ mylib/units/qt_web_render.py
@@ -1,41 +1,41 @@
 #!/usr/bin/env python3
-# encoding=utf8
-from PySide2.QtCore import QUrl
-from PySide2.QtWebEngineWidgets import QWebEnginePage
-from PySide2.QtWidgets import QApplication
+
+from qtpy.QtCore import QUrl
+from qtpy.QtWebEngineWidgets import QWebEnginePage
+from qtpy.QtWidgets import QApplication
 from requests_html import HTML
 
 
-class QtWebPageRender(QWebEnginePage):
-    html: str
+class QtWebPageRender:
     url: str
+    html_str: str
     html_parser: HTML
 
     def __init__(self, qt_app: QApplication):
-        self.app = qt_app
-        QWebEnginePage.__init__(self)
-        self.loadFinished.connect(self._on_load_finished)
-        self.xpath = self.html_parser.xpath
+        self._qt_app = qt_app
+        self._qt_webpage = QWebEnginePage()
+        self._qt_webpage.loadFinished.connect(self._on_load_finished)
+
+    def _on_load_finished(self):
+        self._qt_webpage.toHtml(self._update_html)
+
+    def _update_html(self, qt_webpage_to_html: str):
+        self.html_str = qt_webpage_to_html
+        self.html_parser = HTML(url=self.url, html=self.html_str)
         self.find = self.html_parser.find
+        self.xpath = self.html_parser.xpath
+        self._qt_app.quit()
+
+    def set_url(self, url: str):
+        self.url = url
+        self._qt_webpage.load(QUrl(url))
+        self._qt_app.exec_()
+
+    def set_html(self, html: str, base_url: str = ''):
+        self.url = base_url
+        self._qt_webpage.setHtml(html, QUrl(base_url))
+        self._qt_app.exec_()
 
     @property
     def text(self):
         return self.html_parser.text
-
-    def set_url(self, url: str):
-        self.url = url
-        self.load(QUrl(url))
-        self.app.exec_()
-
-    def set_html(self, html: str, base_url: str = ''):
-        self.url = base_url
-        self.setHtml(html, QUrl(base_url))
-        self.app.exec_()
-
-    def _on_load_finished(self):
-        self.toHtml(self._update_html)
-
-    def _update_html(self, html: str):
-        self.html = html
-        self.html_parser = HTML(url=self.url, html=self.html)
-        self.app.quit()
