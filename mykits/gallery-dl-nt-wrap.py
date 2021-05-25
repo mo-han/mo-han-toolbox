1195166e5907fa27618fa169c8850d1e0390f0a0
mo-han <zmhungrown@gmail.com>
2021-05-25 11:37:47 +0800

    * cookies: danbooru sankaku

--- mykits/gallery-dl-nt-wrap.py
+++ mykits/gallery-dl-nt-wrap.py
@@ -17,11 +17,17 @@ def new_gallery_dl_cmd(*args, **kwargs):
     return cmd
 
 
+def get_cookies_path(middle_name):
+    return fstk.make_path('%cookies_dir%', f'cookies.{middle_name}.txt', env_var=True)
+
+
 def per_site(args: T.List[str]):
     first = args.pop(0)
 
     if first.startswith('/users/') or first.startswith('/artworks/'):
         url = 'https://www.pixiv.net' + first
+    elif first == 'fanbox':
+        url = f'https://{args.pop(0)}.fanbox.cc'
     elif first == 'twitter':
         url = f'https://twitter.com/{args.pop(0)}/media'
     elif first == 'danbooru':
@@ -41,24 +47,32 @@ def per_site(args: T.List[str]):
                                 'filename="pixiv.{id}_p{num}.{date:%Y-%m-%d}.{title}.{extension}"',
                                 'directory=["[{user[name]}] {category} {user[id]}"]']),
                 *args, url]
+    elif 'fanbox.cc' in url:
+        args = [*GLDLCLIArgs(cookies=get_cookies_path('fanbox'),
+                             o=['cookies-update=true', 'videos=true',
+                                'filename="{category}.{creatorId}.{id}_p{num}.{date!S:.10}.{filename}.{extension}"',
+                                'directory=["[{user[name]}] pixiv {user[userId]} {category} {creatorId}"]']),
+                *args, url]
     elif 'twitter.com' in url:
         args = [*GLDLCLIArgs(o=['filename="twitter.{tweet_id}-{num:03d}.{date:%%Y-%%m-%%d}.{filename}.{extension}"',
                                 'directory=["[{author[nick]}] {category} @{author[name]}"]', 'videos=true',
                                 'retweets=false', 'content=true']),
                 *args, url]
     elif 'danbooru.donmai.us' in url:
-        args = [*GLDLCLIArgs(o=['filename="danbooru.{id}.{created_at:.10}.{md5}.'
+        args = [*GLDLCLIArgs(cookies=get_cookies_path('danbooru'),
+                             o=['cookies-update=true', 'videos=true',
+                                'filename="danbooru.{id}.{created_at:.10}.{md5}.'
                                 '{tag_string_character:L64/(various)/}.{extension}"', 'videos=true',
                                 'directory=["{category} {search_tags}"]', ]),
                 *args, url]
     elif 'gelbooru.com' in url:
-        args = [*GLDLCLIArgs(cookies=fstk.make_path('%cookies_dir%/cookies.gelbooru.txt', env_var=True),
+        args = [*GLDLCLIArgs(cookies=get_cookies_path('gelbooru'),
                              o=['cookies-update=true', 'videos=true',
                                 'directory=["{category} {search_tags}"]',
                                 'filename="{category}.{id}.{date!S:.10}.{md5}.{extension}"']),
                 *args, url]
     elif 'sankakucomplex.com' in url:
-        args = [*GLDLCLIArgs(cookies=fstk.make_path('%cookies_dir%/cookies.sankaku.txt', env_var=True),
+        args = [*GLDLCLIArgs(cookies=get_cookies_path('sankaku'),
                              o=['cookies-update=true', 'videos=true',
                                 'directory=["{category} {search_tags}"]',
                                 'filename="{category}.{id}.{date!S:.10}.{md5}.{extension}"']),
