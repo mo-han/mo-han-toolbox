735a38c3325d01fb4c5755f18a6370c7cbafb480
mo-han <zmhungrown@gmail.com>
2021-09-20 23:40:56 +0800

    + ezcolib

---
+++ ezcolib/common/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import glob as glob
+import itertools as itertools
+import pathlib as pathlib
+import queue as queue
+import re as re
+import subprocess as subprocess
+import sys as sys
+import time as time
+
+from ezcolib.stdlib import typing
+
+T = typing
+sleep = time.sleep
+
+
+def __avoid_pycharm_optimize_imports():
+    return queue, re, sys, subprocess, time, pathlib, glob, itertools
---
+++ ezcolib/stdlib/typing.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from typing import *
+from typing import IO, BinaryIO, TextIO  # necessary!
+
+
+def __ref():
+    return IO, BinaryIO, TextIO
+
+
+Decorator = Callable[[Callable], Callable]
+
+
+class QueueType:
+    def put(self, *args, **kwargs):
+        ...
+
+    def get(self, *args, **kwargs):
+        ...
+
+
+JSONType = Union[str, int, float, bool, None, Mapping[str, 'JSON'], List['JSON']]
+
+NoneType = type(None)
+
+EllipsisType = type(Ellipsis)
