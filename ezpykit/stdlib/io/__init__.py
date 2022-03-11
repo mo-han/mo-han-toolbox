c1e63e593ad62b5eeb83e55aa60da40198e14ec8
mo-han <--global>
2022-03-11 16:06:07 +0800

    + virtual file io

--- ezpykit/stdlib/io/__init__.py
+++ ezpykit/stdlib/io/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import builtins
 import os as _os
 from io import *
 
@@ -103,20 +104,91 @@ class IOKit:
             return x.write(*args, **kwargs)
 
 
-class VirtualFileIOWrapper:
-    _instances: T.Dict[T.Tuple, T.Union[StringIO, BytesIO]] = {}
-    obj: T.Union[StringIO, BytesIO]
+class VirtualFileIOBase:
+    def close(self) -> None:
+        self.value = self.getvalue()
+        super(VirtualFileIOBase, self).close()
 
-    def __init__(self, name, mode='r'):
-        key = name, mode
-        if key in self._instances:
-            obj = self._instances[key]
-            if not obj.closed:
-                self.obj = obj
-                return
-        if 'b' in mode:
-            obj = BytesIO()
+    def reopen(self, **kwargs):
+        super(VirtualFileIOBase, self).__init__(self.value, **kwargs)
+        delattr(self, 'value')
+
+    def __init__(self, name, *args, **kwargs):
+        super(VirtualFileIOBase, self).__init__(*args, **kwargs)
+        self.name = name
+
+
+class VirtualTextFileIO(VirtualFileIOBase, StringIO):
+    pass
+
+
+class VirtualBinaryFileIO(VirtualFileIOBase, BytesIO):
+    pass
+
+
+class VirtualFileManager:
+    _files = {}
+
+    @classmethod
+    def new(cls, vfname: str, binary: bool, value=None):
+        if binary:
+            vf = VirtualBinaryFileIO(vfname)
         else:
-            obj = StringIO()
-        self._instances[key] = obj
-        self.obj = obj
+            vf = VirtualTextFileIO(vfname)
+        if value:
+            vf.write(value)
+            vf.seek(0)
+        cls._files[vfname] = vf
+        return vf
+
+    @classmethod
+    def get(cls, vfname, mode: str, **kwargs):
+        if any([i in mode for i in 'ax+']):
+            raise NotImplementedError('mode', mode)
+        want_bin = 'b' in mode
+        if vfname in cls._files:
+            vf = cls._files[vfname]
+            if vf.closed:
+                vf.reopen()
+            if isinstance(vf, VirtualBinaryFileIO):
+                has_bin = True
+            elif isinstance(vf, VirtualTextFileIO):
+                has_bin = False
+            else:
+                return cls.new(vfname, binary=want_bin)
+            if has_bin == want_bin:
+                return vf
+            elif want_bin:
+                return cls.new(vfname, binary=True, value=vf.getvalue().encode(kwargs.get('encoding') or 'utf-8'))
+            else:
+                return cls.new(vfname, binary=False, value=vf.getvalue().decode(kwargs.get('encoding') or 'utf-8'))
+        else:
+            return cls.new(vfname, binary=want_bin)
+
+    @classmethod
+    def set(cls, vfname, vfobj):
+        if not isinstance(vfobj, VirtualFileIOBase):
+            raise TypeError('invalid type as VirtualFileIO', type(vfobj))
+        cls._files[vfname] = vfobj
+
+    @classmethod
+    def rm(cls, vfname):
+        if vfname in cls._files:
+            del cls._files[vfname]
+
+
+def open_virtual_file_io(file, mode='r', **kwargs):
+    if isinstance(file, str) and file.startswith('virtualfile:///'):
+        return VirtualFileManager.get(file, mode, **kwargs)
+    else:
+        return open(
+            file, mode=mode, **kwargs)
+
+
+def replace_open_support_vitual_file_io():
+    open_virtual_file_io.replace = builtins.open
+    builtins.open = open_virtual_file_io
+
+
+def restore_open():
+    builtins.open = open_virtual_file_io.replace
