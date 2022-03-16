e379a419845930cd7d5812ba879907d767245cb9
mo-han <>
2022-03-16 14:02:38 +0800

    *

--- #install.cmd
+++ #install.cmd
@@ -5,11 +5,21 @@ if not defined dst set dst=%default.dst%
 
 pushd "%~dp0"
 pyclean .
+
 mkdir %dst%\mylib
 xcopy /s /y mylib %dst%\mylib
-xcopy /s /y ezpykit %dst%\ezpykit
-xcopy /s /y ezpykitext %dst%\ezpykitext
-xcopy /s /y websites %dst%\websites
+mkdir %dst%\mykits
 xcopy /s /y mykits %dst%
+mkdir %dst%\i18n
 xcopy /s /y i18n %dst%\i18n
+
+mkdir %dst%\ezpykit
+xcopy /s /y ezpykit %dst%\ezpykit
+mkdir %dst%\ezpykitext
+xcopy /s /y ezpykitext %dst%\ezpykitext
+mkdir %dst%\websites
+xcopy /s /y websites %dst%\websites
+mkdir %dst%\apps
+xcopy /s /y apps %dst%\apps
+
 popd
