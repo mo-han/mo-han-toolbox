780c333b069d6a63f14b87a9a4725fef0ebee2fd
mo-han <>
2022-03-27 21:18:07 +0800

    *

--- dota2_waiter.py
+++ dota2_waiter.py
@@ -4,7 +4,7 @@
 """
 
 from dota2_lib import Dota2Controller
-from os.path import realpath, dirname
+from os.path import realpath, dirname, expanduser
 from sys import argv
 
 __author__ = '墨焓 <zmhungrown@gmail.com>'
@@ -12,7 +12,7 @@ __program__ = 'dota2 waiter'
 __version__ = 'demo'
 
 if __name__ == '__main__':
-    feed = dirname(realpath(argv[0])) + '/data/dota2_waiter'
+    feed = expanduser('~/etc/dota2_waiter')
     print('素材文件夹路径', realpath(feed))
     g = Dota2Controller(feed)
     g.auto_waiter((5, 20))
