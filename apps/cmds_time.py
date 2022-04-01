6a3e8aef7e2d36548817acb0cedfa1c89ee3d61a
mo-han <>
2022-04-01 16:41:56 +0800

    + datetime: from_iso_format

---
+++ apps/cmds_time.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from ezpykitext.appkit import *
+
+apr = argparse.ArgumentParserWrapper()
+an = apr.an
+
+an.unix_timestamp = an.iso_datetime = ''
+
+
+@apr.sub(aliases=['u2i'], help='UTC unix timestamp to iso8601')
+@apr.arg(an.unix_timestamp, type=float, nargs='?', default=time.time())
+@apr.map(an.unix_timestamp)
+def unix2iso(t):
+    r = datetime.datetime.utcfromtimestamp(t).isoformat()
+    print(r)
+    return r
+
+
+@apr.sub(aliases=['i2u'], help='UTC iso8601 to unix timestamp')
+@apr.arg(an.iso_datetime)
+@apr.map(an.iso_datetime)
+def iso2unix(s):
+    r = datetime.from_iso_format(s).timestamp()
+    print(r)
+    return r
+
+
+def main():
+    apr.parse()
+    apr.run()
+
+
+if __name__ == '__main__':
+    main()
--- ezpykit/stdlib/datetime.py
+++ ezpykit/stdlib/datetime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from datetime import time, datetime, date, timedelta
+from datetime import *
+import re
 
 
 def datetime_to_delta(time_obj: time):
@@ -8,3 +9,19 @@ def datetime_to_delta(time_obj: time):
 
 def delta_to_datetime(time_delta: timedelta):
     return (datetime.min + time_delta).time()
+
+
+def from_iso_format(s):
+    yr, mon, day, h, m, s, ss, tzs, tzh, tzm = re.match(
+        r'(\d{4})-(\d{2})-(\d{2})[T ](\d{2}):(\d{2}):(\d{2})(\.\d+)?([+-])?(\d{2})?:?(\d{2})?', s).groups()
+    tzs = int((tzs or '') + '1')
+    tzh = int(tzh or 0) * tzs
+    tzm = int(tzm or 0) * tzs
+    if tzh or tzm:
+        tz = timezone(timedelta(hours=tzh, minutes=tzm))
+    else:
+        tz = timezone.utc
+    new: datetime = datetime.min
+    new = new.replace(year=int(yr), month=int(mon), day=int(day), hour=int(h), minute=int(m), second=int(s),
+                      microsecond=int(float(ss or 0) * 1000000), tzinfo=tz)
+    return new
