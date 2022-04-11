87d3a513367c96f2bcebe07b767b463f4afeef3f
mo-han <>
2022-04-11 21:35:22 +0800

    + MatchWrapper

--- ezpykit/stdlib/re.py
+++ ezpykit/stdlib/re.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 from re import *
+from re import __doc__
+
+from ezpykit.metautil import T, DummyObject
+
+___ref = [__doc__]
 
 
 def find_words(s: str, allow_mix_non_word_chars='\''):
@@ -23,3 +28,25 @@ def simple_replace(s, pattern: str, repl: str, *, use_regex=False, ignore_case=F
             return sub(escape(pattern), escape(repl), s, flags=IGNORECASE)
         else:
             return s.replace(pattern, repl)
+
+
+class MatchWrapper:
+    DUMMY_MATCH = DummyObject(match('', ''), group=None)
+
+    def __init__(self, m: T.Match):
+        self.match = m or self.DUMMY_MATCH
+
+    def __getattr__(self, item):
+        return getattr(self.match, item)
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            name, default = item
+            try:
+                return self.match[name]
+            except IndexError:
+                return default
+        return self.match[item]
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} of {self.match!r}'
