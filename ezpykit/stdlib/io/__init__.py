0e870088c7cd31a8926d6e3844a264696df1249e
mo-han <>
2022-05-09 15:44:44 +0800

    Update __init__.py

--- ezpykit/stdlib/io/__init__.py
+++ ezpykit/stdlib/io/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from io import *
-from ezpykit.metautil import typing as T
+from ezpykit.metautil import T
 
 from ezpykit.stdlib.io.slicefileio import *
 from ezpykit.stdlib.io.virtualfileio import *
