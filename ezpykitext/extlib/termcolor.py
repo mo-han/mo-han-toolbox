6734fbb75218ffac29a55ee2354fb733d95d870d
mo-han <>
2022-05-11 13:57:28 +0800

    +

--- ezpykitext/extlib/termcolor.py
+++ ezpykitext/extlib/termcolor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-from ezpykit.allinone import ctx_ensure_module
+from ezpykit.allinone import ctx_ensure_module, logging
+
+__logger__ = logging.get_logger(__name__)
 
 with ctx_ensure_module('colorama'):
     import colorama
@@ -10,3 +12,38 @@ with ctx_ensure_module('termcolor'):
 colorama.init()
 
 ___ref = [cprint]
+
+
+def styled(text, stylesheet=None):
+    kwargs = {}
+    ss = stylesheet
+    if not ss:
+        return colored(text)
+    if isinstance(ss, str):
+        ss = ss.split()
+    ss = list(ss)
+
+    on_colors = []
+    for x in ss:
+        if x.startswith('on'):
+            on_colors.append(x)
+            bg = x[2:].strip('-_')
+            if bg in COLORS.keys():
+                kwargs['on_color'] = bg
+            else:
+                __logger__.warning(f'ignore invalid background color: {bg}')
+
+    attributes = []
+    for x in ss:
+        if x in ATTRIBUTES.keys() and x not in attributes:
+            attributes.append(x)
+    kwargs['attrs'] = attributes
+
+    possible_colors = [e for e in ss if e not in attributes and e not in on_colors]
+    for c in possible_colors:
+        if c in COLORS.keys():
+            kwargs['on_color'] = c
+        else:
+            __logger__.warning(f'ignore invalid color: {c}')
+
+    return colored(text, **kwargs)
