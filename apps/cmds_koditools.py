32e58950068553536fca256a14213cb25c4c9a68
mo-han <>
2022-09-04 14:10:57 +0800

    + make_tvshow_nfo, enum_season_episode_appending, ...

--- apps/cmds_koditools.py
+++ apps/cmds_koditools.py
@@ -6,6 +6,38 @@ from ezpykitext.appkit import *
 __logger__ = logging.get_logger(__name__)
 ap = argparse.ArgumentParserWrapper()
 
+TV_SHOW_NFO_TEMPLATE = '''\
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<!--created on  - tinyMediaManager 4.3.3-->
+<episodedetails>
+  <title></title>
+  <originaltitle/>
+  <showtitle></showtitle>
+  <season>-1</season>
+  <episode>-1</episode>
+  <displayseason>-1</displayseason>
+  <displayepisode>-1</displayepisode>
+  <id/>
+  <ratings/>
+  <userrating>0.0</userrating>
+  <plot></plot>
+  <runtime>0</runtime>
+  <mpaa/>
+  <premiered/>
+  <aired/>
+  <watched>false</watched>
+  <playcount>0</playcount>
+  <trailer/>
+  <dateadded></dateadded>
+  <epbookmark/>
+  <code/>
+  <!--tinyMediaManager meta data-->
+  <source>UNKNOWN</source>
+  <original_filename></original_filename>
+  <user_note/>
+</episodedetails>
+'''
+
 
 class Config:
     dryrun = True
@@ -15,7 +47,7 @@ class Config:
         return bool(cls.dryrun)
 
 
-class KodiNfo:
+class KodiTvShowNfo:
     class InvalidNfoFile(Exception):
         pass
 
@@ -25,19 +57,51 @@ class KodiNfo:
         dirname, stem, ext = os.split_path_dir_stem_ext(fp)
         if ext.lower() != '.nfo':
             raise self.InvalidNfoFile('not a .nfo file', fp)
-        self.xml = ElementTree.parse(fp).getroot()
+        self.xml_etree = ElementTree.parse(fp)
         self.dirname = dirname
         self.stem = stem
         self.ext = ext
 
     @property
-    def nfo_basename(self):
+    def basename(self):
         return self.stem + self.ext
 
-    def update_nfo(self, new_nfo_basename):
+    @property
+    def filename(self):
+        return os.join_path(self.dirname, self.basename)
+
+    @property
+    def episode(self):
+        return int(self.xml_etree.getroot().find('episode').text)
+
+    @episode.setter
+    def episode(self, value):
+        self.xml_etree.getroot().find('episode').text = f'{value:d}'
+
+    @property
+    def season(self):
+        return int(self.xml_etree.getroot().find('season').text)
+
+    @season.setter
+    def season(self, value):
+        self.xml_etree.getroot().find('season').text = f'{value:d}'
+
+    @property
+    def displayepisode(self):
+        return int(self.xml_etree.getroot().find('displayepisode').text)
+
+    @property
+    def displayseason(self):
+        return int(self.xml_etree.getroot().find('displayseason').text)
+
+    def save_nfo(self):
+        self.xml_etree.write(self.filename)
+
+    def set_nfo(self, new_nfo_basename):
         with os.ctx_pushd(self.dirname):
             # self.xml = ElementTree.parse(nfo_basename).getroot()
             self.stem, self.ext = os.split_ext(new_nfo_basename)
+        self.xml_etree = ElementTree.parse(self.filename)
 
     def _check_stem(self, fp):
         stem = os.split_path_dir_stem_ext(fp)[1]
@@ -45,10 +109,10 @@ class KodiNfo:
 
     def get_sidecar_files_basename(self):
         with os.ctx_pushd(self.dirname):
-            return [f for f in os.listdir() if self._check_stem(f) and f != self.nfo_basename]
+            return [f for f in os.listdir() if self._check_stem(f) and f != self.basename]
 
     def get_all_season_x_episode_pattern(self):
-        findall = self.xml.findall
+        findall = self.xml_etree.getroot().findall
         return [f'{int(s.text):d}x{int(e.text):02d}' for s, e in itertools.chain(
             zip(findall('season'), findall('episode')),
             zip(findall('displayseason'), findall('displayepisode')),
@@ -68,10 +132,10 @@ class KodiNfo:
         for old in sidecar_files:
             new = new_basename_maker(old, patterns)
             self._rename(old, new)
-        old_nfo = self.nfo_basename
+        old_nfo = self.basename
         new_nfo = new_basename_maker(old_nfo, patterns)
         self._rename(old_nfo, new_nfo)
-        self.update_nfo(new_nfo)
+        self.set_nfo(new_nfo)
 
     @staticmethod
     def basename_maker_remove_season_x_episode(basename, patterns):
@@ -104,6 +168,43 @@ class KodiNfo:
                 r.append(cls(fp))
         return r
 
+    @classmethod
+    def new_nfo_file(cls, nfo_fp, overwrite=False):
+        if not overwrite and os.path_isfile(nfo_fp):
+            __logger__.debug(f'# skip existing NFO: {nfo_fp}')
+            return nfo_fp
+        io.IOKit.write_exit(open(nfo_fp, 'w', encoding='utf8'), TV_SHOW_NFO_TEMPLATE)
+        o = cls(nfo_fp)
+        root = o.xml_etree.getroot()
+        root.find('plot').text = o.stem
+        root.find('title').text = o.stem
+        o.save_nfo()
+        __logger__.debug(f'+ {o.filename}')
+        return o.filename
+
+    def list_siblings(self):
+        with os.ctx_pushd(self.dirname):
+            season_episode_l = []
+            for p in os.listdir():
+                try:
+                    nfo = self.__class__(p)
+                except self.InvalidNfoFile:
+                    continue
+                for pair in [(nfo.season, nfo.episode), (nfo.displayseason, nfo.displayepisode)]:
+                    if pair >= (1, 1):
+                        season_episode_l.append(pair)
+            return season_episode_l
+
+    def enum_season_episode_appending(self, default_season: int, default_episode: int):
+        siblings = self.list_siblings()
+        if siblings:
+            self.season, self.episode = max(siblings)
+            self.episode += 1
+        else:
+            self.season, self.episode = default_season, default_episode
+        self.save_nfo()
+        return self.season, self.episode
+
 
 @ap.sub()
 @ap.arg('method', choices=('add', 'remove', 'clear',))
@@ -118,9 +219,30 @@ def sxe(method, verbose, dryrun):
         logging.set_root_level('DEBUG')
     elif verbose:
         logging.set_root_level('INFO')
-    m = getattr(KodiNfo, f'basename_maker_{method}_season_x_episode')
-    for x in KodiNfo.map_instances(iter_path()):
+    m = getattr(KodiTvShowNfo, f'basename_maker_{method}_season_x_episode')
+    for x in KodiTvShowNfo.map_instances(iter_path()):
         x.rename_all(m)
+
+
+@ap.sub()
+@ap.arg('season', type=int, help='default season number')
+@ap.arg('episode', type=int, default=11, nargs='?', help='default episode number')
+@ap.opt('v', 'verbose', action='count', default=0)
+@ap.map(season='season', episode='episode', verbose='verbose')
+def make_tvshow_nfo(season, episode, verbose):
+    """create nfo and set season & episode"""
+    logging.init_root(logging.FMT_MESSAGE_ONLY)
+    if verbose >= 2:
+        logging.set_root_level('DEBUG')
+    elif verbose:
+        logging.set_root_level('INFO')
+    nfo_fpl = [KodiTvShowNfo.new_nfo_file(os.split_ext(e)[0] + '.nfo') for e in iter_path()]
+    for x in KodiTvShowNfo.map_instances(nfo_fpl):
+        if (x.season, x.episode) >= (1, 1):
+            __logger__.info(f'# season & episode set already: {x.filename}')
+            continue
+        x.enum_season_episode_appending(season, episode)
+        __logger__.info(f'* {x.season}x{x.episode}: {x.filename}')
 
 
 def main():
