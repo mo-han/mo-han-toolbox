de7c040a0d5bc8c4f4f2c31d82ad832092155225
mo-han <>
2022-09-10 17:31:21 +0800

    * cssselect('[class^=video-desc]')

--- mylib/sites/bilibili/__to_be_deprecated__.py
+++ mylib/sites/bilibili/__to_be_deprecated__.py
@@ -331,7 +331,8 @@ class YouGetBilibiliX(you_get.extractors.bilibili.Bilibili):
         h = self.html_etree
         desc = seq_call_return((
             {'target': lambda: h.xpath('//div[@id="v_desc"]')[0].text_content()},
-            {'target': lambda: h.xpath('//meta[@name="description"]')[0].attrib['content']},
+            # {'target': lambda: h.xpath('//meta[@name="description"]')[0].attrib['content']},
+            {'target': lambda: h.cssselect('[class^=video-desc]')[0].text},
             {'target': lambda: h.cssselect('.video-desc')[0].text},
             {'target': lambda: h.cssselect('.media-desc.webkit-ellipsis')[0].text},
         ))
